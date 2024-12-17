0858844396541fff2404205550ce70a2626d9a4e
ap-ctrl <ankitaslump@gmail.com>
2024-12-18 00:43:44 +0530

    New file

---
+++ mini.java
@@ -0,0 +1,11 @@
+package locak;
+
+public class mini {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+}
